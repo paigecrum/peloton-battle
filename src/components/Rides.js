37c3a47dc32c2c211e05173aa3b429be394da075
Paige Crum <paigecrum@gmail.com>
2021-03-14 18:23:38 -0500

    abstract ride list into grid component, update default ride length on mount

--- src/components/Rides.js
+++ src/components/Rides.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-// import { getRides } from '../utils/api'
+import { getRides } from '../utils/api'
 
 function RideLengthNav({selected, onUpdateRideLength}) {
-  const rideLengths = ['All', '20 min', '30 min', '45 min', '60 min'];
+  const rideLengths = ['All', '15 min', '20 min', '30 min', '45 min', '60 min'];
 
   return (
     <ul className='flex-center'>
@@ -14,7 +14,7 @@ function RideLengthNav({selected, onUpdateRideLength}) {
             className='btn-clear nav-link'
             style={ 
               rideLength === selected
-              ? { color: '#5393fe'}
+              ? { color: 'hsl(218,99%,66%)'}
               : null
             }
             onClick={() => onUpdateRideLength(rideLength)}>
@@ -31,25 +31,69 @@ RideLengthNav.propTypes = {
   onUpdateRideLength: PropTypes.func.isRequired
 }
 
+function RidesGrid({ rides }) {
+  return (
+    <div>
+      <ul className='grid space-around'>
+      { rides.map((ride) => (
+        <li key={ride.id}>
+          <div className='card'>
+            <img
+              className='card-test'
+              src={`${ride.imageUrl}`}
+              alt='Thumbnail from selected ride.'
+            />
+            <h4>{ride.title}</h4>
+            <p>{ride.instructor}</p>
+            <p>{`${ride.numFriends} friends have taken this class.`}</p>
+            <p>{`Taken on ${ride.date}`}</p>
+          </div>
+        </li>
+      ))}
+      </ul>
+    </div>
+  )
+}
+
+RidesGrid.propTypes = {
+  rides: PropTypes.array.isRequired
+}
+
 export default class Rides extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      selectedRideLength: 'All'
+      selectedRideLength: 'All',
+      rides: {}
     }
 
     this.updateRideLength = this.updateRideLength.bind(this)
+  }
+
+  componentDidMount() {
+    this.updateRideLength(this.state.selectedRideLength)
   }
 
   updateRideLength(selectedRideLength) {
     this.setState({
       selectedRideLength
     })
+
+    if (!this.state.rides[selectedRideLength]) {
+      this.setState(({ rides }) => {
+        return {
+          rides: {
+            ...rides,
+            [selectedRideLength]: getRides(selectedRideLength)
+          }
+        }
+      })
+    }
   }
 
   render() {
-    const { selectedRideLength } = this.state
+    const { selectedRideLength, rides } = this.state;
 
     return (
       <React.Fragment>
@@ -58,6 +102,7 @@ export default class Rides extends React.Component {
           onUpdateRideLength={ this.updateRideLength }
         />
         <p className='flex-center'>Select a ride you've taken to battle a friend.</p>
+        { rides[selectedRideLength] && <RidesGrid rides={rides[selectedRideLength]} /> }
       </React.Fragment>
     )
   }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,88 @@
 
 // mock ride data for now
-const data = {
-  rides: []
+const data1 = {
+  metadata: 'sup',
+  rides: [
+    {
+      'id': '1',
+      'title': '20 min Rock Ride',
+      'instructor': 'Denis Morton',
+      'date': 'Monday',
+      'friends': 2,
+      'imageUrl': 'https://s3.amazonaws.com/peloton-ride-images/0ebd52d1d0abae68768aa0819be2a9a61b3fe98f/img_1603377871_f56d1496b4084fcf9d181a1b92ff1edb.jpg'
+    },
+    {
+      'id': '2',
+      'title': '45 min Pop Ride',
+      'instructor': 'Cody Rigsby',
+      'date': 'Tuesday',
+      'friends': 4,
+      'imageUrl': 'https://s3.amazonaws.com/peloton-ride-images/cca38e04c2bde7db300ab80351e79d7bd9723c37/img_1612279517_6513185b3722401e8a8923540a3c8156.png'
+    },
+  ]
 }
 
-function getRides() {
+const data2 = {
+  metadata: 'sup',
+  rides: [
+    {
+      'id': '1',
+      'title': '20 min Rock Ride',
+      'instructor': 'Denis Morton',
+      'date': 'Monday',
+      'numFriends': 2,
+      'imageUrl': 'https://s3.amazonaws.com/peloton-ride-images/325eddbac12bcd9e2991b4007e8e266c8945746c/img_1605807056_bbe3b42dc0ee4aff9ecde4e54313548e.jpg'
+    },
+    {
+      'id': '2',
+      'title': '45 min Pop Ride',
+      'instructor': 'Cody Rigsby',
+      'date': 'Tuesday',
+      'numFriends': 4,
+      'imageUrl': 'https://s3.amazonaws.com/peloton-ride-images/1a5663cb898b2553cef24aeb49d9b80582390057/img_1607014873_6b6363399f7d4aa1956c356b03e33817.png'
+    },
+    {
+      'id': '3',
+      'title': '30 min Classic Rock Ride',
+      'instructor': 'Denis Morton',
+      'date': 'Monday',
+      'numFriends': 2,
+      'imageUrl': 'https://s3.amazonaws.com/peloton-ride-images/325eddbac12bcd9e2991b4007e8e266c8945746c/img_1605807056_bbe3b42dc0ee4aff9ecde4e54313548e.jpg'
+    },
+    {
+      'id': '4',
+      'title': '45 min Britney Ride',
+      'instructor': 'Cody Rigsby',
+      'date': 'Tuesday',
+      'numFriends': 4,
+      'imageUrl': 'https://s3.amazonaws.com/peloton-ride-images/1a5663cb898b2553cef24aeb49d9b80582390057/img_1607014873_6b6363399f7d4aa1956c356b03e33817.png'
+    },
+    {
+      'id': '5',
+      'title': '15 min Power Zone Endurance Ride',
+      'instructor': 'Denis Morton',
+      'date': 'Monday',
+      'numFriends': 2,
+      'imageUrl': 'https://s3.amazonaws.com/peloton-ride-images/325eddbac12bcd9e2991b4007e8e266c8945746c/img_1605807056_bbe3b42dc0ee4aff9ecde4e54313548e.jpg'
+    },
+    {
+      'id': '6',
+      'title': '6',
+      'instructor': 'Cody Rigsby',
+      'date': 'Tuesday',
+      'numFriends': 4,
+      'imageUrl': 'https://s3.amazonaws.com/peloton-ride-images/1a5663cb898b2553cef24aeb49d9b80582390057/img_1607014873_6b6363399f7d4aa1956c356b03e33817.png'
+    }
+  ]
+}
+
+export function getRides(rideLength) {
   // TODO: Replace with API Gateway call
 
-  return data;
+  if (rideLength === '20 min') {
+    return data1.rides;
+  } else {
+    return data2.rides;
+  }
+
 }
