0544a2544fea59ab9b5f9125e3e99c08d3dbe441
Paige Crum <paigecrum@gmail.com>
2021-04-08 20:27:28 -0500

    convert Loading to functional component with hooks

--- ui/src/components/Loading.js
+++ ui/src/components/Loading.js
@@ -1,43 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Box, Spinner, Text } from 'grommet'
 import { Bike } from 'grommet-icons'
 
+export default function Loading({ text = 'Loading', speed = 300}) {
+  const [content, setContent] = useState(text);
 
-export default class Loading extends React.Component {
-  state = { text: this.props.text }
-
-  static propTypes = {
-    text: PropTypes.string.isRequired,
-    speed: PropTypes.number.isRequired
-  }
-
-  static defaultProps = {
-    text: 'Loading',
-    speed: 300
-  }
-
-  componentDidMount() {
-    const { text: customText, speed } = this.props
-
-    this.interval = window.setInterval(() => {
-      this.state.text === customText + '...'
-      ? this.setState({ text: customText })
-      : this.setState(({ text }) => ({ text: text + '.'}))
+  useEffect(() => {
+    const id = window.setInterval(() => {
+      setContent((content) => {
+        return content === `${text}...`
+          ? text
+          : `${content}.`
+      })
     }, speed)
-  }
-  componentWillUnmount() {
-    window.clearInterval(this.interval)
-  }
-  render() {
-    return (
-      <Box align='center' direction='row' gap='medium' margin={{ vertical: 'medium' }}>
-        <Spinner />
-        <Text alignSelf='center' size='large'>
-          { this.state.text }
-        </Text>
-        <Bike />
-      </Box>
-    )
-  }
+
+    return () => window.clearInterval(id)
+  }, [text, speed])
+
+  return (
+    <Box align='center' direction='row' gap='medium' margin={{ vertical: 'medium' }}>
+      <Spinner />
+      <Text alignSelf='center' size='large'>
+        { content }
+      </Text>
+      <Bike />
+    </Box>
+  )
+}
+
+Loading.propTypes = {
+  text: PropTypes.string,
+  speed: PropTypes.number
 }
