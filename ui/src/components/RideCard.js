6d01a170f9573d15641d894a00d72a0679040892
Paige Crum <paigecrum@gmail.com>
2021-05-02 12:29:06 -0500

    refactor RideCard animations and styling approach

--- ui/src/components/RideCard.js
+++ ui/src/components/RideCard.js
@@ -1,77 +1,91 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useLayoutEffect, useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
-import { Box, Card, CardBody, CardHeader, Image, Stack, Text } from 'grommet'
+import { Box, Card, CardBody, CardFooter, Stack, Text } from 'grommet'
 import { Calendar, User } from 'grommet-icons'
-import "styled-components/macro"
+import 'styled-components/macro'
 
 import { instructorMap, formatDate } from '../utils/helpers'
 
-const StyledLink = styled(Link)`
-  text-decoration: none;
-`;
-
 export default function RideCard({ ride }) {
   const [hovering, setHovering] = useState(false);
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+  const timeoutRef = useRef(0);
+  const history = useHistory();
+
+  useLayoutEffect(() => {
+    timeoutRef.current = setTimeout(() => {
+      clearTimeout(timeoutRef.current);
+
+      if (hovering) {
+        setShouldAnimate(true);
+      } else {
+        setShouldAnimate(false);
+      }
+    }, 10);
+
+    return () => {
+      clearTimeout(timeoutRef);
+    }
+
+  }, [shouldAnimate, hovering]);
+
+  const handleClickCard = () => {
+    history.push(`/battle/${ride.id}`, { ride });
+  }
 
   return (
-    <StyledLink
-      to={{
-        pathname: `/battle/${ride.id}`,
-        state: { ride }
-      }}
+    <ImageCard
+      height='234px'
+      width='418px'
+      src={ride.imageUrl}
+      animation={shouldAnimate ? {type: 'zoomIn', size: 'small', duration: 500} : undefined}
+      onClick={handleClickCard}
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
     >
-      <Card
-        height='234px'
+      <CardBody />
+      <CardFooter
+        pad={{ horizontal: 'small', vertical: 'xsmall' }}
+        background='#000000A0'
         width='418px'
-        animation={hovering ? {type: 'zoomIn'/*, delay: 1000*/} : null}
-        onMouseOver={() => setHovering(true)}
-        onMouseOut={() => setHovering(false)}
+        justify='between'
       >
-        <Stack anchor='bottom-left'>
-          <CardBody height='234px'>
-            <Image
-              fit='cover'
-              src={ride.imageUrl}
-              a11yTitle='Thumbnail from selected ride.'
-            />
-          </CardBody>
-          <CardHeader
-            pad={{ horizontal: 'small', vertical: 'xsmall' }}
-            background='#000000A0'
-            width='418px'
-            justify='between'
-          >
-            <Box>
-              <Text size='16px' weight={600}  margin='none'>
-                {ride.title}
+        <Box>
+          <Text size='16px' weight={600}  margin='none'>
+            {ride.title}
+          </Text>
+          <Text size='small'>{instructorMap[ride.instructorId]}</Text>
+          <Box direction='row' align='center'>
+            <Calendar size='small' />
+            <Text size='xsmall' margin={{ horizontal: 'xsmall'}}>
+              {formatDate(ride.classStartTimestamp)}
+            </Text>
+          </Box>
+        </Box>
+        <Box alignSelf='end'>
+          <Stack anchor='top-right'>
+            <User size='30px' />
+            <Box
+              background='brand'
+              responsive={false}
+              align='center'
+              height='15px'
+              width='15px'
+              round
+            >
+              <Text color='white' size='11px' css='position: relative; top: 1px'>
+                {ride.numFriends}
               </Text>
-              <Text size='small'>{instructorMap[ride.instructorId]}</Text>
-              <Box direction='row' align='center'>
-                <Calendar size='small' />
-                <Text size='xsmall' margin={{ horizontal: 'xsmall'}}>
-                  {formatDate(ride.classStartTimestamp)}
-                </Text>
-              </Box>
             </Box>
-            <Stack anchor='top-right'>
-              <User size='30px' />
-              <Box
-                background='brand'
-                responsive={false}
-                align='center'
-                height='15px'
-                width='15px'
-                round
-              >
-                <Text color='white' size='11px' css='position: relative; top: 1px'>
-                  {ride.numFriends}
-                </Text>
-              </Box>
-            </Stack>
-          </CardHeader>
-        </Stack>
-      </Card>
-    </StyledLink>
+          </Stack>
+        </Box>
+      </CardFooter>
+    </ImageCard>
   )
 }
+
+const ImageCard = styled(Card)`
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+`;
