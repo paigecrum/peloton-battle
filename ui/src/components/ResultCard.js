aea0ec47250626356fe03403162e2b4e0b10b1a8
Paige Crum <paigecrum@gmail.com>
2021-05-03 23:42:28 -0500

    results card styling updates

--- ui/src/components/ResultCard.js
+++ ui/src/components/ResultCard.js
@@ -6,33 +6,35 @@ import { formatDate } from '../utils/helpers'
 
 export default function ResultCard({ player, outcome }) {
   return (
-    <Card height='medium' width='medium' margin='small' background='light-1'>
-      <CardHeader justify='center'>
-        <Heading level='2' size='small' margin={{ top: 'medium', bottom: 'xsmall' }}>{outcome}</Heading>
+    <Card height='250px' width='250px' margin='large' align='center' background='light-1' overflow='visible'>
+      <CardHeader  margin='-50px'>
+        <Avatar src={player.avatarUrl} size='100px'/>
       </CardHeader>
-      <CardBody align='center' justify='center'>
-        <Avatar src={player.avatarUrl} size='3xl' />
-        <Box align='center' margin='xsmall'>
-          <Heading margin='none' level='3' size='small'>
-            {player.username}
-          </Heading>
-          <Box pad={{ top: 'xsmall' }} align='center'>
-            { player.stats.summaries.map((stat) => (
-              <Text
-                key={stat.slug}
-                size={stat.slug === 'total_output' ? '16px' : 'small'}
-                margin={stat.slug === 'total_output' ? { vertical: 'xsmall' } : '0px'}
-              >
-                <b>{stat.display_name}:</b> {stat.value} {stat.display_unit}
-              </Text>
-            ))}
+      <CardBody align='center' justify='center' width='220px' pad={{ top: 'large' }}>
+        <Heading level='3' size='xsmall' margin='none'>
+          {player.username}
+        </Heading>
+        <Box pad={{ top: 'xsmall' }} align='center'>
+          { player.stats.summaries.map((stat) => (
+            <Text
+              key={stat.slug}
+              size={stat.slug === 'total_output' ? '16px' : 'small'}
+              margin={stat.slug === 'total_output' ? { vertical: 'xsmall' } : '0px'}
+            >
+              <b>{stat.display_name}:</b> {stat.value} {stat.display_unit}
+            </Text>
+          ))}
+          <Box pad={{ top: 'small' }}>
+            <Text size='small'>
+              {formatDate(player.startedClassAt)}
+            </Text>
           </Box>
         </Box>
       </CardBody>
-      <CardFooter justify='center' pad={{ bottom: 'medium' }}>
-        <Text size='small'>
-          {formatDate(player.startedClassAt)}
-        </Text>
+      <CardFooter width='250px' pad={{ vertical: 'xsmall' }} justify='center' background='light-3'>
+        <Heading level='3' size='small' margin='none'>
+          {outcome}
+        </Heading>
       </CardFooter>
     </Card>
   )
